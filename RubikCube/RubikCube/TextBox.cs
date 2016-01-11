ba8299f31a4f6805326d5a4f9552fa171740e5e6
Denis The Hagmon <denisgurarie@gmail.com>
2016-01-11 17:45:21 +0200

    TextBox 90% DONE!!!!!
    - there's still a small problem with DELETE where movedTo = -1 somhow.
    - finnaly added a physical tab "|" !!!!
    -everything is awsome!

--- RubikCube/RubikCube/TextBox.cs
+++ RubikCube/RubikCube/TextBox.cs
@@ -20,9 +20,13 @@ namespace RubikCube
     {
         public string textbox = "";
         public string drawBox = "";
+        public string finaleDrawBox = "";
         public string tabString = "";
+        int drawTab = 0;
+        string physTab = "";
         public int movedTo = 0;
         int timeSincePress = 0;
+        int tabTimer = 0;
         public const int boxSize = 20;
         public int tabPlace = 0;
         public TextBox()
@@ -31,6 +35,7 @@ namespace RubikCube
 
         public void Update(KeyboardState state, KeyboardState oldState, GameTime gameTime)
         {
+            tabTimer += gameTime.ElapsedGameTime.Milliseconds % 1000;
             //34
             //(Keys)(Enum.Parse(typeof(Keys), "A"));
             CheckForClick(ref state, ref oldState, Keys.A);
@@ -66,9 +71,10 @@ namespace RubikCube
                 textbox += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             }
             //cheaks if one of the keys that count how much time has passed since you pressed them have been...um...Un-pressed?
-            if (((state.IsKeyUp(Keys.Right)) && (oldState.IsKeyDown(Keys.Right)))||((state.IsKeyUp(Keys.Left))&&(oldState.IsKeyDown(Keys.Left)))||((state.IsKeyUp(Keys.Back))&&(oldState.IsKeyDown(Keys.Back))))
+            if (((state.IsKeyUp(Keys.Right)) && (oldState.IsKeyDown(Keys.Right))) || ((state.IsKeyUp(Keys.Left)) && (oldState.IsKeyDown(Keys.Left))) || ((state.IsKeyUp(Keys.Back)) && (oldState.IsKeyDown(Keys.Back))))
             {
                 timeSincePress = 0;
+                tabTimer = 0;
             }
 
             if ((state.IsKeyDown(Keys.Back)) && (textbox.Length > 0))
@@ -82,23 +88,34 @@ namespace RubikCube
                     }
                     if ((tabPlace + movedTo) > 0)
                     {
-                        if ((textbox.Length -boxSize -movedTo >0)&&)
+                        if (textbox.Length <= boxSize)
                         {
                             tabPlace--;
                         }
-                        else if (tabPlace<boxSize)
+                        if ((MovedToRight() > 0 && (movedTo > 0)))
                         {
-                            tabPlace--;
-                        }
-                        if (movedTo > 0)
-                        {
-                            if (textbox.Length - boxSize - movedTo ==0)
+                            if (tabPlace > 0)
+                            {
+                                tabPlace--;
+                            }
+                            else
                             {
                                 movedTo--;
                             }
-                           
                         }
-                        textbox = textbox.Remove(movedTo + tabPlace);
+                        if (MovedToRight() > 0 && (movedTo == 0))
+                        {
+                            tabPlace--;
+                        }
+                        if ((MovedToRight() == 0) && (movedTo > 0))
+                        {
+                            movedTo--;
+                        }
+                        {
+
+                        }
+                        textbox = textbox.Remove(movedTo + tabPlace, 1);
+                        tabTimer = 0;
                     }
 
                 }
@@ -108,20 +125,26 @@ namespace RubikCube
                 timeSincePress += gameTime.ElapsedGameTime.Milliseconds;
                 if ((oldState.IsKeyUp(Keys.Right) || (timeSincePress > 250)))
                 {
-                    if (timeSincePress > 250)
+                    if (!((tabPlace == boxSize)&&(MovedToRight()==0)))
                     {
-                        timeSincePress = 225;
-                    }
-                    if ((tabPlace == boxSize) && (textbox.Length - boxSize - movedTo > 0))
-                    {
-                        movedTo++;
-                    }
-                    else if ((tabPlace < boxSize) && (textbox.Length - tabPlace > 0))
-                    {
-                        tabPlace++;
+                        if (timeSincePress > 250)
+                        {
+                            timeSincePress = 225;
+                        }
+                        if ((tabPlace == boxSize) && (MovedToRight() > 0))
+                        {
+                            movedTo++;
+                        }
+                        else if ((tabPlace < boxSize) && (textbox.Length - tabPlace > 0))
+                        {
+                            tabPlace++;
+                        }
+                        tabTimer = 0;
                     }
                     else
+                    {
                         Console.Beep();
+                    }
                 }
             }
             if (state.IsKeyDown(Keys.Left))
@@ -129,24 +152,25 @@ namespace RubikCube
                 timeSincePress += gameTime.ElapsedGameTime.Milliseconds;
                 if ((oldState.IsKeyUp(Keys.Left) || (timeSincePress > 250)))
                 {
-                    if (timeSincePress > 250)
+                    if (movedTo + tabPlace > 0)
                     {
-                        timeSincePress = 225;
-                    }
-                    if (tabPlace > 0)
-                    {
-                        tabPlace--;
-                    }
-                    if (tabPlace == 0)
-                    {
-                        if (movedTo > 0)
+                        if (timeSincePress > 250)
+                        {
+                            timeSincePress = 225;
+                        }
+                        if (tabPlace > 0)
+                        {
+                            tabPlace--;
+                        }
+                        if (tabPlace == 0)
                         {
                             movedTo--;
                         }
-                        else
-                        {
-                            Console.Beep();
-                        }
+                        tabTimer = 0;
+                    }
+                    else
+                    {
+                        Console.Beep();
                     }
 
                 }
@@ -160,44 +184,60 @@ namespace RubikCube
             {
                 drawBox = textbox;
             }
-            tabString = "";
-            for (int i = 0; i < tabPlace; i++)
+            finaleDrawBox = "";
+            finaleDrawBox = string.Join(" ", drawBox.ToCharArray());
+            drawTab = tabPlace * 2;
+            if (tabPlace > 0)
             {
-                tabString += "  ";
+                drawTab--;
+            }
+
+            if (tabTimer < 500)
+            {
+                physTab = "|";
+            }
+            else
+            {
+                physTab = "";
+            }
+            if(tabTimer >= 1000)
+            {
+                tabTimer = 0;
             }
             oldState = state;
+        }
+
+        private int MovedToRight()
+        {
+            return textbox.Length - boxSize - movedTo;
         }
         private void CheckForClick(ref KeyboardState keyboardState, ref KeyboardState oldKeyboardState, Keys key)
         {
             if (keyboardState.IsKeyDown(key) && oldKeyboardState.IsKeyUp(key))
             {
-                //   if (((keyboardState.IsKeyDown(Keys.RightShift)) || (keyboardState.IsKeyDown(Keys.LeftShift))) && ((oldKeyboardState.IsKeyUp(Keys.RightShift)) || (oldKeyboardState.IsKeyUp(Keys.LeftShift))))
-                // {
-                //   textbox += KeyToChar(key, keyboardState, oldKeyboardState);
-                // textbox += "i";
-                //}
-                //else
-                //{
-                textbox = textbox.Insert(movedTo + tabPlace,KeyToChar(key, keyboardState, oldKeyboardState));
+                textbox = textbox.Insert(movedTo + tabPlace, KeyToChar(key, keyboardState, oldKeyboardState));
                 if (tabPlace < boxSize)
                 {
                     tabPlace++;
                 }
-                if ((tabPlace >= boxSize) && (textbox.Length - boxSize - movedTo > 0))
+                if ((tabPlace >= boxSize) && (MovedToRight() > 0))
                 {
                     tabPlace = boxSize;
                     movedTo++;
                 }
-                //}
             }
         }
 
         public void Draw(SpriteBatch spriteBatch, SpriteFont font)
         {
             spriteBatch.Begin();
-            spriteBatch.DrawString(font, ("TimeSincePress: " + timeSincePress), new Vector2(300, 350), Color.Black);
-            spriteBatch.DrawString(font, ("Text: " + tabString + ","), new Vector2(300, 375), Color.Black);
-            spriteBatch.DrawString(font, ("Text: " + drawBox), new Vector2(300, 375), Color.Black);
+            spriteBatch.DrawString(font, ("tabtime: " + tabTimer), new Vector2(300, 350), Color.Black);
+            if (tabPlace == 0)
+                spriteBatch.DrawString(font, ("Text:" + (finaleDrawBox.Substring(0, drawTab)) + physTab), new Vector2(300, 375), Color.Gray);
+            else
+                spriteBatch.DrawString(font, ("Text: " + (finaleDrawBox.Substring(0, drawTab)) + physTab), new Vector2(300, 375), Color.Gray);
+            spriteBatch.DrawString(font, ("Text: " + finaleDrawBox), new Vector2(300, 375), Color.Black);
+            //spriteBatch.DrawString(font, ("Text: " + (finaleDrawBox.Substring(test))), new Vector2(300, 375), Color.Black);
             spriteBatch.DrawString(font, ("Length: " + textbox.Length), new Vector2(300, 400), Color.Black);
             spriteBatch.DrawString(font, ("MovedTo: " + movedTo), new Vector2(300, 425), Color.Black);
             spriteBatch.DrawString(font, ("TabPlace: " + tabPlace), new Vector2(300, 450), Color.Black);
